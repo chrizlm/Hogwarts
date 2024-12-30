d4a72a3c457340742704639057eb1e2c37c8f5ad
christopher.lijoodi <christopher.lijoodi@turnkeyafrica.com>
2024-12-30 14:17:53 +0300

    Add observability stack (Prometheus, Grafana, Zipkin and Mailpit) to the project

--- src/main/java/com/hogwats/online/artifact/ArtifactService.java
+++ src/main/java/com/hogwats/online/artifact/ArtifactService.java
@@ -4,6 +4,7 @@ import com.hogwats.online.artifact.dto.ArtifactDto;
 import com.hogwats.online.artifact.utils.IdWorker;
 import com.hogwats.online.system.exception.ObjectNotFoundException;
 import io.micrometer.core.annotation.Timed;
+import io.micrometer.observation.annotation.Observed;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,7 @@ public class ArtifactService {
         this.idWorker = idWorker;
     }
 
+    @Observed(name = "artifact", contextualName = "findByIdService")
     public Artifact findById(String artifactId){
         return this.artifactRepository.findById(artifactId)
                 .orElseThrow(()-> new ObjectNotFoundException("Artifact",artifactId));
